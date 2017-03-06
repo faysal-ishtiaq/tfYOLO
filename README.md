5354038baa952790c7f8995eda7922915d0a4462
faysal-ishtiaq <f.i.rabby@gmail.com>
2017-03-06 12:35:00 +0600

    "initial commit"

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Project Status: Concept - Minimal or no implementation has been done yet.](http://www.repostatus.org/badges/latest/concept.svg)](http://www.repostatus.org/#concept)
+
 # tfYOLO
 TensorFlow implementation of YOLO (You Only Look Once)
---
+++ main.py
@@ -0,0 +1,26 @@
+__author__ = "f.i.rabby@gmail.com"
+
+import tensorflow as tf
+import numpy as np
+import cv2
+
+def readCamera():
+    '''
+    TODO: detect objects from camera feed
+    :return: string
+    '''
+
+def readImage():
+    '''
+    TODO: detect objects from image
+    :return: string
+    '''
+
+def main():
+    '''
+    TODO: main work
+    :return: string
+    '''
+
+if __name__ == "__main__":
+    main()
